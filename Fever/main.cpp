cc4d8951643c76c106831949c068bbaefdf20c17
TaroMilky <133554200+TaroMilky@users.noreply.github.com>
2025-04-16 23:06:04 -0400

    Initial Commit

---
+++ Fever/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Fever
+//
+//  Created by Laurence Ipo on 4/16/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
